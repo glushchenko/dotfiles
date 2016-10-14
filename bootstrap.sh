8ee5dd452723eaad7950b7d2316af3787e1e03af
Oleksandr Glushchenko <contact@fluder.co>
2016-10-14 20:55:48 +0300

    Bundles implementation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
 # script to symlink files in ~/.dotfiles to the home directory
 
-files=~/.dotfiles/.*
-ignorefiles=(".gitignore" ".gitmodules")
+IGNORE_LINK=(".gitignore" ".gitmodules")
+DOTFILES_DIR="$HOME/.dotfiles/"
 
-for full_path in $files
+if [ -z $1 ]; then
+    files=$DOTFILES_DIR.*
+    echo "All configs prepared for install."
+else
+    bundleLinks=()
+    lines=`cat $DOTFILES_DIR/bundle.$1`
+    for line in $lines; do
+        bundleLinks+=("$DOTFILES_DIR$line");
+    done
+    files=${bundleLinks[@]}
+    echo "Bundle: $1"
+fi
+
+successLinkedQty=0
+
+for linkedPath in $files
 do
-    file=`echo $full_path | cut -d'/' -f 5`
-    if [[ ${ignorefiles[*]} =~ "$file" ]]
+    file=`echo $linkedPath | cut -d'/' -f 5`
+    if [[ ${IGNORE_LINK[*]} =~ "$file" ]]
     then
 		echo "$file passed (ignored file list)"
     else
-        if [ ! -h ~/$file ]
+        if [ ! -e ~/$file ]
         then
             echo "Creating symlink for $file"
-            ln -s $full_path ~/$file
+            ln -s $linkedPath ~/$file
+            successLinkedQty=$((successLinkedQty+1))
         else
             echo "Config already exists for $file"
         fi
     fi
 done
- 
-echo "Finished creating symlinks."
 
-# update submodules
+echo "Successfuly linked: $successLinkedQty"
+
+# Update git submodules if exist
 git submodule init
 git submodule update
-
-echo "Submodules updated."
